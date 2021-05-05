5011522a0053fe14e9c458dba559ea9ac8ea96a9
almazov03 <almazov.03@list.ru>
2021-05-05 17:47:45 +0300

    Added user-friendly interface

--- bot.py
+++ bot.py
@@ -6,10 +6,13 @@ TOKEN = open('key.txt').read()
 def start(bot, update):
     bot.message.reply_text("Привет! Напиши мне начало стиха, а я его продолжу!")
 def Make_continue(bot, update):
-    with open("log.txt", "a") as fl:
-        s =("User: " + str(bot.message.chat.username) + ', ' + str(bot.message.chat.first_name) + ', ' + str(bot.message.chat.last_name) + \
-                  " Message: " + str(bot.message.text))
-        fl.writelines(s + '\n')
+    try:
+        with open("log.txt", "a") as fl:
+            s =("User: " + str(bot.message.chat.username) + ', ' + str(bot.message.chat.first_name) + ', ' + str(bot.message.chat.last_name) + \
+             " Message: " + str(bot.message.text))
+            fl.writelines(s + '\n')
+    except:
+        pass
     bot.message.reply_text(make_continue_markovify(bot.message.text))
 
 def main():
