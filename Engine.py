506604de6504e3569007714936b50ceab76f0519
Kamil <ninevskykn@gmail.com>
2021-05-05 17:13:28 +0300

    Added engine

---
+++ Engine.py
@@ -0,0 +1,18 @@
+import markovify
+import json
+
+with open('model_json.json') as fl:
+    model_json = json.load(fl)
+    text_model = markovify.Text.from_json(model_json)
+
+def make_continue_markovify(start):
+    start = start.lower()
+    try:
+        return text_model.make_sentence_with_start(start).replace(' _ ', '\n')
+    except:
+        return start + ' ' + text_model.make_sentence().replace(' _ ', '\n')
+
+
+start = input()
+print(start)
+print(make_continue_markovify(start))
