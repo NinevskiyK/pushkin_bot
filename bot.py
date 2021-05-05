a50788c14360e960fc2993a164a880ec024229ea
almazov03 <almazov.03@list.ru>
2021-05-05 17:36:39 +0300

    Added logs

--- bot.py
+++ bot.py
@@ -7,8 +7,9 @@ def start(bot, update):
     bot.message.reply_text("Hello. Write Text.")
 def Make_continue(bot, update):
     with open("log.txt", "a") as fl:
-        fl.append("User: " + str(bot.message.chat.username) + ', ' + str(bot.message.chat.first_name) + ', ' + str(bot.message.chat.last_name) + \
+        s =("User: " + str(bot.message.chat.username) + ', ' + str(bot.message.chat.first_name) + ', ' + str(bot.message.chat.last_name) + \
                   "Message: " + str(bot.message.text))
+        fl.writelines(s + '\n')
     bot.message.reply_text(make_continue_markovify(bot.message.text))
 
 def main():
